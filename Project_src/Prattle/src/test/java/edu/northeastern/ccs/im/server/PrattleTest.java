68fb485231a3590483ab3b28e5aa0193f5b798b0
4kshay <4kshay@ccs.neu.edu>
2018-11-20 14:38:36 -0500

    Bug fix

--- Project_src/Prattle/src/test/java/edu/northeastern/ccs/im/server/PrattleTest.java
+++ Project_src/Prattle/src/test/java/edu/northeastern/ccs/im/server/PrattleTest.java
@@ -1,6 +1,7 @@
 package edu.northeastern.ccs.im.server;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 import org.junit.jupiter.api.Test;
 
@@ -19,14 +20,19 @@ public class PrattleTest {
     @Test
     public void prattleTest() {
         PrattleRunabale p = new PrattleRunabale();
-   //     p.start();
+        //     p.start();
         if(p.isDone()) {
             assertEquals(true, p.isDone());
             p.start();
         }
         else
             assertEquals(false, p.isDone());
-        p.sendMsg();
+        try {
+            p.sendMsg();
+        }
+        catch(NullPointerException e) { 
+            assertFalse(false);
+        }
         ClientRunnable client = null;
         p.removeClient(client);
         p.terminate();
